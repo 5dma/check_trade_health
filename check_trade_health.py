1016b31b99cdda5d45192a2ea9ca4306dc75a121
5dma <mark@lautman.net>
2022-12-04 10:02:57 -0500

    Stopping parsing, add dictionary for command line arguments

--- check_trade_health.py
+++ check_trade_health.py
@@ -9,36 +9,58 @@ import sys
 import requests
 from html.parser import HTMLParser
 from datetime import datetime
+from enum import Enum
+
+class Column(Enum):
+	Date = 0
+	Open = 1
+	High = 2
+	Low = 3
+	Close = 4
+	AdjClose = 5
+	Volume = 6
+
+class Direction(Enum):
+	Long = 0
+	Short = 1
 
 class MyHTMLParser(HTMLParser):
-	write_data = False
+	in_span = False
 	in_tbody = False
 	span_counter = 0
 	def handle_starttag(self, tag, attrs):
 		if (tag == 'tbody'):
 			self.in_tbody = True
+		if (tag == 'tr') and self.in_tbody:
 			self.span_counter = 0
 		if (tag == 'span'):
-			self.write_data = True
+			self.in_span = True
 			#print("Start tag:", tag)
 
 	def handle_endtag(self, tag):
 		if (tag == 'tbody'):
 			self.in_tbody = False
 		if (tag == 'span'):
-			self.write_data = False
-			self.span_counter  += 1
+			self.in_span = False
+			self.span_counter += 1
 		#print("End tag  :", tag)
 
 	def handle_data(self, data):
-		if (self.in_tbody and self.write_data):
-			print("Self {0}".format(self.span_counter))
-			if (self.span_counter % 7 == 0):
+		if (self.in_tbody and self.in_span):
+			if ((self.span_counter % 7) == Column.Date.value):
+				#print("Self {0} {1}".format(self.span_counter, data))
 				current_date = datetime.strptime(data,"%b %d, %Y")
 				current_date_epoch = current_date.timestamp()
-				if (current_date_epoch >= buy_date_epoch):
+				if (current_date_epoch >= trade_info["buy_date_epoch"]):
 					print("Date     :", data)
+				else:
+					sys.exit()
 
+			#if ((self.span_counter % 7) == Column.Low.value):
+			#	current_date = datetime.strptime(data,"%b %d, %Y")
+			#	current_date_epoch = current_date.timestamp()
+			#	if (current_date_epoch >= buy_date_epoch):
+			#		print("Date     :", data)
 	#def handle_comment(self, data):
 		#print("Comment  :", data)
 
@@ -63,12 +85,22 @@ html_file = open('apple.html')
 r = html_file.read()
 html_file.close()
 
-buy_date = datetime.strptime("11/28/2022","%m/%d/%Y")
-buy_date_epoch = buy_date.timestamp()
-print(buy_date_epoch)
+
+
+trade_info = {
+	"buy_date": "11/03/2022",
+	"direction": Direction.Long,
+	"stop": 142.00,
+	"target": 150.00
+}
+
+buy_date = datetime.strptime(trade_info["buy_date"],"%m/%d/%Y")
+trade_info["buy_date_epoch"] = buy_date.timestamp()
+print(trade_info["buy_date_epoch"])
 
 parser = MyHTMLParser()
 parser.feed(r)
+parser.close()
 
 
 
