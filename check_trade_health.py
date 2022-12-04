7739f5e10a0b8d1cda6a914dacb7a5684b050c3a
5dma <mark@lautman.net>
2022-12-03 21:27:33 -0500

    Request works.

---
+++ check_trade_health.py
@@ -0,0 +1,13 @@
+#
+#Symbol Start-Date, Direction, Open, Target, Stop, 
+#
+#
+#
+#MANU	11/28/22	Short	20.04	15.28	21.63	21.63	-1.59	Gap up too high
+
+import sys
+import requests
+r = requests.get('https://httpbin.org/')
+print(r.text)
+
+print("Hello")
