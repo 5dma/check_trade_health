563af18d44e9230c3bd1545c0ee05886b70674ea
5dma <mark@lautman.net>
2022-12-11 13:06:41 -0500

    Added logic to find earliest event.

--- check_trade_health.py
+++ check_trade_health.py
@@ -26,11 +26,18 @@ class Direction(Enum):
 	Long = 0
 	Short = 1
 
+class Outcome(Enum):
+	Open = 0
+	Sold = 1
+	Stopped = 2
+	Both = 3
+
 class MyHTMLParser(HTMLParser):
 	in_span = False
 	in_tbody = False
 	span_counter = 0
 	continue_processing = True
+	current_date_epoch = 0
 	def handle_starttag(self, tag, attrs):
 		if self.continue_processing:
 			if (tag == 'tbody'):
@@ -41,43 +48,64 @@ class MyHTMLParser(HTMLParser):
 				self.in_span = True
 
 	def handle_endtag(self, tag):
-		if self.continue_processing:
-			if (tag == 'tbody'):
-				self.in_tbody = False
+		if (tag == 'tbody'):
+			self.in_tbody = False
+			print("Trade results:")
+			if (trade_info["sell_date"] == None):
+				print("Trade is still active")
+			else:
+				if (trade_info["direction"] == Direction.Short) and (trade_info["status"] == Outcome.Stopped):
+					print("  Trade closed on {:s} at {:.2f} ({:.2f}).".format(datetime.fromtimestamp(trade_info["sell_date"]).strftime("%m/%d/%Y"), trade_info["stop"], trade_info["sell_price"]))
+				if (trade_info["direction"] == Direction.Short) and (trade_info["status"] == Outcome.Sold):
+					print("  Trade closed on {:s} at {:.2f} ({:.2f}).".format(datetime.fromtimestamp(trade_info["sell_date"]).strftime("%m/%d/%Y"), trade_info["target"], trade_info["sell_price"]))
+				if (trade_info["direction"] == Direction.Long) and (trade_info["status"] == Outcome.Stopped):
+					print("  Trade closed on {:s} at {:.2f} ({:.2f}).".format(datetime.fromtimestamp(trade_info["sell_date"]).strftime("%m/%d/%Y"), trade_info["stop"], trade_info["sell_price"]))
+				if (trade_info["direction"] == Direction.Long) and (trade_info["status"] == Outcome.Sold):
+					print("  Trade closed on {:s} at {:.2f} ({:.2f}).".format(datetime.fromtimestamp(trade_info["sell_date"]).strftime("%m/%d/%Y"), trade_info["target"], trade_info["sell_price"]))
+				
+		elif self.continue_processing:
 			if (tag == 'span'):
 				self.in_span = False
 				self.span_counter += 1
 
 	def handle_data(self, data):
 		if (self.in_tbody and self.in_span and self.continue_processing):
+
 			if ((self.span_counter % 7) == Column.Date.value):
+				#print("Processing date {0} {1}".format(data, trade_info["buy_date"]))
 				current_date = datetime.strptime(data,"%b %d, %Y")
-				current_date_epoch = current_date.timestamp()
-				if (current_date_epoch >= trade_info["buy_date_epoch"]):
-					trade_info["current_date"] = current_date
-				else:
-					print("Trade is still active")
+				self.current_date_epoch = current_date.timestamp()
+				if (self.current_date_epoch <= trade_info["buy_date_epoch"]):
 					self.continue_processing = False
 
 			if ((self.span_counter % 7) == Column.Low.value):
 				if trade_info["direction"] == Direction.Long:
-					if (float(data) < trade_info["stop"]):
-						print("Stopped at {0} on {1} (low was {2})".format(trade_info["stop"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
-						self.continue_processing = False
+					if (float(data) <= trade_info["stop"]):
+						trade_info["sell_date"] = self.current_date_epoch
+						trade_info["status"] = Outcome.Stopped
+						trade_info["sell_price"] = float(data)
+						#print("Stopped at {0} on {1} (low was {2})".format(trade_info["stop"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
+						
 				else: # Direction is short
 					if (float(data) <= trade_info["target"]):
-						print("Sold at {0} on {1} (low was {2})".format(trade_info["target"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
-						self.continue_processing = False
+						#print("Sold at {0} on {1} (low was {2})".format(trade_info["target"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
+						trade_info["sell_date"] = self.current_date_epoch
+						trade_info["status"] = Outcome.Sold
+						trade_info["sell_price"] = float(data)
 
 			if ((self.span_counter % 7) == Column.High.value):
 				if trade_info["direction"] == Direction.Long:
 					if (float(data) >= trade_info["target"]):
-						print("Sold at {0} on {1} (high was {2})".format(trade_info["target"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
-						self.continue_processing = False
+						#print("Sold at {0} on {1} (high was {2})".format(trade_info["target"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
+						trade_info["sell_date"] = self.current_date_epoch
+						trade_info["status"] = Outcome.Sold
+						trade_info["sell_price"] = float(data)
 				else: # Direction is short
 					if (float(data) >= trade_info["stop"]):
-						print("Stopped at {0} on {1} (high was {2})".format(trade_info["stop"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
-						self.continue_processing = False
+						#print("Stopped at {0} on {1} (high was {2})".format(trade_info["stop"], trade_info["current_date"].strftime("%Y-%m-%d"), data))
+						trade_info["sell_date"] = self.current_date_epoch
+						trade_info["status"] = Outcome.Stopped
+						trade_info["sell_price"] = float(data)
 
 
 
@@ -109,15 +137,20 @@ def evaluate_trade(trade_info):
 	parser.close()
 
 
-with open('/tmp/more_experiments.csv', mode='r') as file:
+trade_info = {}
+with open('more_experiments.csv', mode='r') as file:
 	csv_file = csv.DictReader(file,None,None, dialect='unix', delimiter='\t', quoting=csv.QUOTE_ALL)
 	for lines in csv_file:
+		trade_info.clear()
 		trade_info = {
 			"symbol": lines["Symbol"],
 			"buy_date": lines["Buy date"],
 			"direction": Direction.Short if (lines["Direction"] == "Short") else Direction.Long,
 			"stop": float(lines["Stop"]),
-			"target": float(lines["Target"])
+			"target": float(lines["Target"]),
+			"status": Outcome.Open,
+			"sell_date": None,
+			"sell_price": 0
 		}
 		evaluate_trade(trade_info)
 		#if (lines["Sold"] != ''):
